a78da5bba3e01ea9a57b3798598f519e0e66c7fd
TamPereira <89050340+TamPereira@users.noreply.github.com>
2022-01-18 14:43:57 -0300

    git initnovo projeto

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'filmflix-2b804',
+    appId: '1:878078330312:web:728d7f1994f558168d6cfd',
+    storageBucket: 'filmflix-2b804.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyAjVPqHXGbrq7zuU_lwFMBAqFvc7qnGfqk',
+    authDomain: 'filmflix-2b804.firebaseapp.com',
+    messagingSenderId: '878078330312',
+  },
   production: false
 };
 
